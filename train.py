626c3a381b9e8a9df30d86f5dd48b9670755b63f
Tianyi <mrbaoziii@gmail.com>
2020-08-11 15:44:33 +0800

    fix normalization

--- train.py
+++ train.py
@@ -194,7 +194,7 @@ if __name__ == "__main__":
 
     print('\n', " Loading Data ".center(50, "="), sep='')
     adata = load_data(args)
-    adata = normalize_data(adata)
+    adata = adata if arg.batch_correction != "none" else normalize_data(adata)
     filtered_dataset = SingleCellDataset(adata)
 
     # Initial PCA
